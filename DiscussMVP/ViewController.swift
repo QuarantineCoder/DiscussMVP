14389c9d7c5a13d873ec867fe3e2488253a540d7
Sahil <SahilP@duck.com>
2022-08-05 08:25:03 -0400

    Completed MVP for Discuss

--- DiscussMVP/ViewController.swift
+++ DiscussMVP/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    var radius = 10
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        usernameTextField.layer.cornerRadius = CGFloat(radius)
+        passwordTextField.layer.cornerRadius = CGFloat(radius)
+        signInButton.layer.cornerRadius = CGFloat(radius)
+        signUpButton.layer.cornerRadius = CGFloat(radius)
+        
+        
     }
-
-
 }
 
